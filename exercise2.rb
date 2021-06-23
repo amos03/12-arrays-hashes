2b90a8323fdeb26f71c7104e46f52ae97dbd7729
amos03 <amossi@gmail.com>
2021-06-23 12:52:35 -0400

    fixed grocery list method so that array is outside of method and supplied as argument

--- exercise2.rb
+++ exercise2.rb
@@ -1,10 +1,13 @@
-def print_list
 grocery_list = ["tomatoes", "apples", "peaches", "oranges", "salmon", "bread", "chicken thighs", "potatoes", "milk"]
-puts "My Grocery List"
-grocery_list.each do |grocery_item|
-    puts "* #{grocery_item}"
+grocery_item = 0
+
+def print_list(list, item)
+    
+    puts "My Grocery List"
+list.each do |item|
+    puts "* #{item}"
 end
 end
 
-print_list
+print_list(grocery_list, grocery_item)
 
