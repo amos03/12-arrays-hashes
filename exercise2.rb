cd8e524d2712092ed09b8ef74732e83b95508593
amos03 <amossi@gmail.com>
2021-06-23 12:18:49 -0400

    Exercise 2 grocery list bulleted with asterisk output

---
+++ exercise2.rb
@@ -0,0 +1,5 @@
+grocery_list = ["tomatoes", "apples", "peaches", "oranges", "salmon", "bread", "chicken thighs", "potatoes", "milk"]
+puts "My Grocery List"
+grocery_list.each do |grocery_item|
+    puts "* #{grocery_item}"
+end
