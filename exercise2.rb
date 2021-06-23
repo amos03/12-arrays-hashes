a1873daadf0a72bf79f5509afee5d9a56f594b80
amos03 <amossi@gmail.com>
2021-06-23 12:44:04 -0400

    turned grocery array output into method

--- exercise2.rb
+++ exercise2.rb
@@ -1,5 +1,10 @@
+def print_list
 grocery_list = ["tomatoes", "apples", "peaches", "oranges", "salmon", "bread", "chicken thighs", "potatoes", "milk"]
 puts "My Grocery List"
 grocery_list.each do |grocery_item|
     puts "* #{grocery_item}"
 end
+end
+
+print_list
+
