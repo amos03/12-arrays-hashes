680ddbfb69563167d49b3c4d6978aa46c454f0d2
amos03 <amossi@gmail.com>
2021-06-23 12:02:00 -0400

    Exercise 1 sum of arrays

---
+++ exercise1.rb
@@ -0,0 +1,10 @@
+expenses_2020 = [2.99, 3.99, 10.99, 1, 155]
+expenses_2019 = [11, 9, 22, 18, 40]
+
+
+def total_expenses(expenses_array)
+    p expenses_array.sum
+end
+
+total_expenses(expenses_2020)
+total_expenses(expenses_2019)
