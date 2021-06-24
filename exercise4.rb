0f24df90a4fd54e32e78e6062fab3be8a4594664
amos03 <amossi@gmail.com>
2021-06-24 11:37:35 -0400

    Completed function calling neighbor's dogs. End Exercise 4

--- exercise4.rb
+++ exercise4.rb
@@ -12,12 +12,29 @@ def get_absent_dogs (dogs_array)
             absent_dogs.push(dog[:name])
         end
     end
-puts absent_dogs
-
-absent_dogs.each do |absent_dog|
-    puts "Come back, #{absent_dog}!"
-end
+p "The missings dogs are #{absent_dogs}"
 
 end
 
 get_absent_dogs (my_dogs)
+puts "------------------------------------------------"
+
+puts "Calling the neighbor's missing dogs"
+neighbor_dogs = [
+    {name: "Kelev", position: 5},
+    {name: "Chatul", position: 8},
+    {name: "Kof", position: 11},
+    {name: "Dov", position: 12} 
+] 
+
+# calling absent dogs function
+
+def call_absent_dogs (dogs_array)
+    dogs_array.each do |dog|
+        if dog[:position] > 10
+            puts "Come back, #{dog[:name]}!"
+        end
+end
+end
+
+call_absent_dogs (neighbor_dogs)
