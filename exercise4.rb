5aa383d7458c17875c1f438bf8139124cbdba8ee
amos03 <amossi@gmail.com>
2021-06-24 11:24:36 -0400

    Put absent dogs in an array. Output array. Call each absent dog. But the calling part is not a separate method - trouble with local vs. global variables. variable scope issue.

--- exercise4.rb
+++ exercise4.rb
@@ -5,18 +5,19 @@ my_dogs = [
     {name: "Spot", position: 12} 
 ]
 
-
-
-
 def get_absent_dogs (dogs_array)
     absent_dogs=[]
     dogs_array.each do |dog|
         if dog[:position] > 10
             absent_dogs.push(dog[:name])
-
         end
     end
-p absent_dogs
+puts absent_dogs
+
+absent_dogs.each do |absent_dog|
+    puts "Come back, #{absent_dog}!"
+end
+
 end
 
 get_absent_dogs (my_dogs)
